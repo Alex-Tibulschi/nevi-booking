74f3175f11e568eca57048a041393a5dbd5139c3
Alex-Tibulschi <gabakejsi@yahoo.com>
2025-10-03 23:57:38 +0100

    v0.1.0/1 - started initializing IaC with Terraform connected to AWS

---
+++ infra/README.md
@@ -0,0 +1,5 @@
+default region: eu-west-2
+
+dev profile: salon-dev
+
+Secrets live in local dev.auto.tfvars or AWS Secrets Manager, never commit them...
---
+++ infra/_authcheck/providers.tf
@@ -0,0 +1,8 @@
+terraform {
+  required_providers {
+    aws = { source = "hashicorp/aws", version = "~> 5.60" }
+  }
+}
+provider "aws" {
+  region = "eu-west-2" # your region
+}
