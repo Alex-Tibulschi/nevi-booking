10cca193f2de5a48f19265b547d34762e5658299
Alex-Tibulschi <gabakejsi@yahoo.com>
2025-10-04 13:06:45 +0100

    v0.1.0/4 added default route table to IGW

--- infra/network/main.tf
+++ infra/network/main.tf
@@ -3,7 +3,7 @@ locals {
 }
 
 
-
+# VPC 1
 resource "aws_vpc" "this" {
     cidr_block = var.vpc_cidr
     enable_dns_support = true
@@ -25,7 +25,7 @@ output "vpc_cidr" {
 }
 
 
-
+# Subnet 1 attached to VPC1
 resource "aws_subnet" "subnet1" {
   vpc_id = aws_vpc.this.id
   cidr_block = var.subnet1_cidr
@@ -41,9 +41,52 @@ resource "aws_subnet" "subnet1" {
   }
 }
 
-output "subnet1_id" {
+output "publlic_subnet1_id" {
   value = aws_subnet.subnet1.id
 }
-output "subnet1_cidr" {
+output "public_subnet1_cidr" {
   value = aws_subnet.subnet1.cidr_block
+}
+
+# Internet Gateway attached to VPC1
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "${local.name_prefix}-igw"
+    Project = var.project
+    Env = "dev"
+  }
+}
+
+output "igw_id" {
+  value = aws_internet_gateway.igw.id
+}
+
+# Route table for public access
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "${local.name_prefix}-rt-public"
+    Project = var.project
+    Env = "dev"
+  }
+}
+
+output "public_rt_id" {
+  value = aws_route_table.public.id
+}
+
+# Default route - 0.0.0.0/0 to the IGW
+resource "aws_route" "public_default" {
+  route_table_id = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.igw.id
+}
+
+# Associate route table to subnet
+resource "aws_route_table_association" "subnet1_assoc" {
+  subnet_id = aws_subnet.subnet1.id
+  route_table_id = aws_route_table.public.id
 }
